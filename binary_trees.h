241e9c2cabb72a48d1dceb61bbe7d509cb35f066
Klaus KIRA <klauskira07@gmail.com>
2022-11-18 16:48:44 -0700

    Update Prototypes

--- binary_trees.h
+++ binary_trees.h
@@ -48,6 +48,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 
 /* VOID FUNCT*/
 void binary_tree_delete(binary_tree_t *tree);
